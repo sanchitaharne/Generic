75842884c9338a17470dc4fea30c616461d689c2
sanchita <sanchita.241@gmail.com>
2022-06-15 21:53:22 +0530

    add

---
+++ src/com/generic/practice/LinkedListVsArrayList.java
@@ -0,0 +1,31 @@
+package com.generic.practice;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class LinkedListVsArrayList {
+    public static void main(String[] args) {
+        LinkedList<String> namesLinkedList = new LinkedList<>();
+
+        namesLinkedList.add("sanchita");
+        namesLinkedList.add("nithya");
+        namesLinkedList.add("prasad");
+
+        System.out.println(namesLinkedList.get(2));
+        namesLinkedList.add(2, "akshara");
+
+        ArrayList<String> namesArrayList = new ArrayList<>();
+        namesArrayList.add("sanchita");
+        namesArrayList.add("nithya");
+        namesArrayList.add("prasad");
+
+        System.out.println(namesArrayList.get(2));
+        namesLinkedList.add(2, "akshara");
+
+        //arraylist is effecient in traversing the array and get the element but way slower than linked list while adding a new element
+        //LinkedList is effecient in adding new elements
+
+
+    }
+
+}
